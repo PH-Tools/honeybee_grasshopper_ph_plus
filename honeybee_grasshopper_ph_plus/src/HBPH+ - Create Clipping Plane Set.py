951bdc9727754332592d7cc3530534569ac5c74e
PH Tools <phtools@bldgtyp.com>
2026-03-28 17:00:28 -0400

    Update components

--- honeybee_grasshopper_ph_plus/src/HBPH+ - Create Clipping Plane Set.py
+++ honeybee_grasshopper_ph_plus/src/HBPH+ - Create Clipping Plane Set.py
@@ -49,12 +49,12 @@ import Grasshopper as gh
 try:
     from ph_gh_component_io import gh_io
 except ImportError as e:
-    raise ImportError('Failed to import ph_gh_component_io:\t{}'.format(e))
+    raise ImportError('\nFailed to import ph_gh_component_io:\n\t{}'.format(e))
 
 try:
     from honeybee_ph_plus_rhino import gh_compo_io
 except ImportError as e:
-    raise ImportError('Failed to import honeybee_ph_plus_rhino:\t{}'.format(e))
+    raise ImportError('\nFailed to import honeybee_ph_plus_rhino:\n\t{}'.format(e))
 
 
 # ------------------------------------------------------------------------------
